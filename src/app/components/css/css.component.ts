3960ae887ec5b88ed0edde36dfe6e20edc5af8c9
dktomaszenko <xjxxwbm9fx>
2017-09-18 16:02:46 +0200

    añadimos css.component

---
+++ src/app/components/css/css.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  template: `
+    <p>
+      css Works!
+    </p>
+  `,
+  styles: []
+})
+export class CssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
