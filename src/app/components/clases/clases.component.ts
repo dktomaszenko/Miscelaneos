0ef0a4cada144884a12569ef5a8efc04ed173549
dktomaszenko <xjxxwbm9fx>
2017-09-19 12:38:00 +0200

    creando ngClass

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-clases',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesComponent implements OnInit {
 
-  constructor() { }
+  alerta = 'alert-danger';
+  propiedades: object = {
+    danger: false
+  };
+  loading: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ejecutar() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+
   }
 
 }
